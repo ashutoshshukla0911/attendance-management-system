d33de7a4cca6c8e13cf4feb0780945de54d091f2
ashutoshshukla0911 <shuklaashu2003@gmail.com>
2025-06-03 11:43:17 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String name;
+    private String status;
+
+    public Student(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
