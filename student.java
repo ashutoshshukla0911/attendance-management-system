35948e0c2f33e4240303c71136a6d3e72d6f9f3a
ashutoshshukla0911 <shuklaashu2003@gmail.com>
2025-06-03 12:15:47 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,17 +1,1 @@
-public class Student {
-    private String name;
-    private String status;
 
-    public Student(String name, String status) {
-        this.name = name;
-        this.status = status;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-}
